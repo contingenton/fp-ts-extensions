665cd65915d1697bf3d96470b49b3246adbad820
paolopiaggio <paolo.piaggesi@gmail.com>
2022-12-01 19:46:13 +0100

    taskEitherChainTap

--- packages/fp-ts-extensions/src/lib/fp-ts-extensions.ts
+++ packages/fp-ts-extensions/src/lib/fp-ts-extensions.ts
@@ -3,18 +3,22 @@ import { pipe } from 'fp-ts/function';
 
 export const errorOnNone =
   <A>(
-    error: () => Error,
-  ): ((result: taskEither.TaskEither<Error, option.Option<A>>) => taskEither.TaskEither<Error, A>) =>
-    (result: taskEither.TaskEither<Error, option.Option<A>>): taskEither.TaskEither<Error, A> =>
-      pipe(
-        result,
-        taskEither.chain(
-          option.match(
-            () => taskEither.left(error()),
-            (a) => taskEither.right(a),
-          ),
-        ),
-      );
+    error: () => Error
+  ): ((
+    result: taskEither.TaskEither<Error, option.Option<A>>
+  ) => taskEither.TaskEither<Error, A>) =>
+  (
+    result: taskEither.TaskEither<Error, option.Option<A>>
+  ): taskEither.TaskEither<Error, A> =>
+    pipe(
+      result,
+      taskEither.chain(
+        option.match(
+          () => taskEither.left(error()),
+          (a) => taskEither.right(a)
+        )
+      )
+    );
 
 const toResult = <E, T>(result: taskEither.TaskEither<E, T>) =>
   pipe(
@@ -23,57 +27,66 @@ const toResult = <E, T>(result: taskEither.TaskEither<E, T>) =>
       (e: E) => {
         throw e;
       },
-      (a: T) => a,
-    ),
+      (a: T) => a
+    )
   );
 
-export const taskEitherToPromise: <E, T>(p: taskEither.TaskEither<E, T>) => Promise<T> = <E, T>(
-  p: taskEither.TaskEither<E, T>,
-) => pipe(p, toResult)();
+export const taskEitherToPromise: <E, T>(
+  p: taskEither.TaskEither<E, T>
+) => Promise<T> = <E, T>(p: taskEither.TaskEither<E, T>) => pipe(p, toResult)();
 
-export const tryCatchError: <A>(f: () => Promise<A>) => taskEither.TaskEither<Error, A> = <A>(f: () => Promise<A>) =>
+export const tryCatchError: <A>(
+  f: () => Promise<A>
+) => taskEither.TaskEither<Error, A> = <A>(f: () => Promise<A>) =>
   taskEither.tryCatch(f, either.toError);
 
 export const taskEitherTap =
   <E, A>(action: (right: A) => void) =>
-    (te: taskEither.TaskEither<E, A>) =>
-      pipe(
-        te,
-        taskEither.map((a: A) => {
-          action(a);
-          return a;
-        }),
-      );
+  (te: taskEither.TaskEither<E, A>) =>
+    pipe(
+      te,
+      taskEither.map((a: A) => {
+        action(a);
+        return a;
+      })
+    );
 
 export const taskEitherChainTap =
-  <E, A, B>(taskEitherToChain: ()=> taskEither.TaskEither<E, B>) =>
-    (te: taskEither.TaskEither<E, A>) =>
-      pipe(
-        te,
-        taskEither.chain((a:A)=> pipe(
+  <E1, A, E2, B>(taskEitherToChain: () => taskEither.TaskEither<E2, B>) =>
+  (te: taskEither.TaskEither<E1, A>) =>
+    pipe(
+      te,
+      taskEither.chainW((a: A) =>
+        pipe(
           taskEitherToChain(),
-          taskEither.map((b:B)=> a)
-        ))
-      );
+          taskEither.map((b: B) => a)
+        )
+      )
+    );
 
 export const taskEitherTapLeft =
   <E, A>(action: (left: E) => void) =>
-    (te: taskEither.TaskEither<E, A>) =>
-      pipe(
-        te,
-        taskEither.mapLeft((e: E) => {
-          action(e);
-          return e;
-        }),
-      );
+  (te: taskEither.TaskEither<E, A>) =>
+    pipe(
+      te,
+      taskEither.mapLeft((e: E) => {
+        action(e);
+        return e;
+      })
+    );
 
-export const logError = (logger: (message: string, ...meta: any[]) => unknown) => (error: Error | unknown) => {
-  const e = error instanceof Error ? error : new Error(`Unknown error: ${JSON.stringify(error)}`);
-  logger('An error occurred {error}', {
-    error: {
-      message: e.message,
-      stack: e.stack,
-    },
-  });
-  return e;
-};
+export const logError =
+  (logger: (message: string, ...meta: any[]) => unknown) =>
+  (error: Error | unknown) => {
+    const e =
+      error instanceof Error
+        ? error
+        : new Error(`Unknown error: ${JSON.stringify(error)}`);
+    logger('An error occurred {error}', {
+      error: {
+        message: e.message,
+        stack: e.stack,
+      },
+    });
+    return e;
+  };
